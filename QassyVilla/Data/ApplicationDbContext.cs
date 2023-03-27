8a2f0a74212bef11218a48e52f382708a999923a
Fasasi Qasim <fasaqa1000@gmail.com>
2023-03-27 13:21:40 +0100

    added a database model

---
+++ QassyVilla/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QassyVilla.Models;
+
+namespace QassyVilla.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
