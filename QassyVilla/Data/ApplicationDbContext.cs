ec91e2d71a4b02d51a7576370c88bc132527ed76
Fasasi Qasim <fasaqa1000@gmail.com>
2023-03-28 09:44:25 +0100

    added database

--- QassyVilla/Data/ApplicationDbContext.cs
+++ QassyVilla/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace QassyVilla.Data
 {
     public class ApplicationDbContext :DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { } 
         public DbSet<Villa> Villas { get; set; }
     }
 }
--- QassyVilla/Program.cs
+++ QassyVilla/Program.cs
@@ -1,9 +1,15 @@
 
 
+using Microsoft.EntityFrameworkCore;
+using QassyVilla.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers(option =>
 {
 
